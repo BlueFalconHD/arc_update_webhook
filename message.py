8204a3e5a993ab65d3f5e7c79580c899051fb54b
bluefalconhd <hjdombro@icloud.com>
2023-07-28 00:56:05 -0500

    feat: add funny phrases

--- message.py
+++ message.py
@@ -1,8 +1,21 @@
 from message_builder import *
 from typing import List
 import arc_update_utils
+import random
 
 changelogURL = "https://arc.net/e/D25B2EEA-7506-4850-A169-3B2A00802889"
+
+updatePhrases = [
+    "Beep boop. Hey look, a wild update appeared!",
+    "Beep boop. An update has been spotted!",
+    "Beep boop. An update has been detected!",
+    "Beep boop. An update has been found!",
+    "Mom get the camera! Arc just got updated!",
+    "Mom get the camera! Arc just got updated again!",
+    "Hey look! Arc has been updated",
+    "*robot noises* Arc has been updated",
+    "Arc updated! Also try Minecraft.."
+]
 
 def changelogBulletList(changelog: List) -> str:
     return "\n".join([f"> - {item}" for item in changelog])
@@ -25,7 +38,8 @@ def BuildMessage(updateitem: arc_update_utils.Item) -> str:
     easelText = emojibullet("easel", link("Release notes", changelogURL))
 
     # combine
-    final = f"""{titleText}
+    final = f"""{random.choice(updatePhrases)}
+{titleText}
 
 {changelogText}
 
